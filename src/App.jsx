8e86428aca2cec517d547dd1145e41d9ff3948b4
Suhani Sharma <suhani190505@gmail.com>
2025-08-15 02:02:23 +0530

    successfully completed the navbar hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navbar from './Components/Navbar.jsx';
 import Hero from './Components/Hero.jsx';
 import About from './Components/About.jsx';
-const App=()=> {
+
+const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden' >
-      <Hero/>
-      <About/>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
+      <Navbar />
+      <Hero />
+      <About />
     </main>
   );
 }
